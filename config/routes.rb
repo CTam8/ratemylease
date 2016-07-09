f587b642ae23fd4225f76f97c10b79d69d674696
ChristianMTam <christian.tam@ryerson.ca>
2016-07-09 14:56:24 -0400

    signin using username or email

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 root 'static_pages#home'
-get 'findalease'   => 'static_pages#findalease'
+
+devise_scope :user do
+  get "login", :to  => "devise/sessions#new"
+  get "signup", :to  => "devise/registrations#new"
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20160709023243) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   default: "", null: false
+    t.string   "username",               default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160709023243) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
