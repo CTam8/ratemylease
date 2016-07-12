e9c1135edeb26c09b069ec6991b3972afbf4a6f8
TheMikeL <mike.l_42@hotmail.com>
2016-07-12 19:29:07 -0400

    Deleted username function, moving along with facebook log in. Authorization is set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :username, presence: true, uniqueness: { case_sensitive: false}
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :confirmable, :validatable, :authentication_keys => [:login]
-  devise  :omniauthable, :omniauth_providers => [:facebook]
+  #validates :username, presence: true, uniqueness: { case_sensitive: false}
+  devise :database_authenticatable, :omniauthable, :registerable, :recoverable, :rememberable, :trackable, :confirmable, :validatable, :authentication_keys => [:login]
   #Authenticate using email or username
   def login=(login)
     @login = login
   end
 
-  def self.from_omniauth(auth)
+def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.provider = auth.provider
     user.email = auth.info.email
-    user.name = auth.info.name
     user.password = Devise.friendly_token[0,20]
-    user.username = auth.info.username
   end
 end
 
@@ -28,23 +25,23 @@ def self.new_with_session(params, session)
   end
 
   def login
-    @login || self.username || self.email
+    @login = self.email
   end
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).where("lower(email) = :value", { :value => login.downcase }).first
+    elsif conditions.has_key?(:email)
       where(conditions.to_h).first
     end
   end
-
+=begin
   validate :validate_username
 
   def validate_username
     if User.where(email: username).exists?
       errors.add(:username, :invalid)
   end
-end
+=end
 end
