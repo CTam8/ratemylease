41f301a6a36f9ae0d41b441fb0b8e686dda8f619
ChristianMTam <christian.tam@ryerson.ca>
2016-07-19 19:04:27 -0400

    modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20160719225150) do
 
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_identities_on_user_id"
+  end
+
   create_table "leases", force: :cascade do |t|
     t.string   "address"
     t.string   "province"
@@ -35,26 +44,6 @@ ActiveRecord::Schema.define(version: 20160719225150) do
     t.integer "user_id"
     t.index ["lease_id"], name: "index_reviews_on_lease_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-ActiveRecord::Schema.define(version: 20160719185246) do
-
-  create_table "fbusers", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "oauth_token"
-    t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
