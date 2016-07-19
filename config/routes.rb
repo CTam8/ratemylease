0588cc831da576e3f87e38cb93cfa23fd3f459a7
TheMikeL <mike.l_42@hotmail.com>
2016-07-19 18:47:42 -0400

    Fixed auto generated errors, omniauth is up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
-  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_up => "signup"}
-=======
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
->>>>>>> facebook-auth
-=======
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
->>>>>>> facebook-auth
+
   resources :users
-root 'static_pages#home'
+  root 'static_pages#home'
 
-devise_scope :user do
-  get "login", :to  => "devise/sessions#new"
-  get "signup", :to  => "devise/registrations#new"
-  delete 'sign_out', :to => 'devise/sessions#destroy'
-end
+  devise_scope :user do
+    get "login", :to  => "devise/sessions#new"
+    get "signup", :to  => "devise/registrations#new"
+    delete 'sign_out', :to => 'devise/sessions#destroy'
+  end
 
-match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
 end
