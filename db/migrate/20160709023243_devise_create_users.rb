f49c5b836755d876a3292118e7f4b1d62ab44946
TheMikeL <mike.l_42@hotmail.com>
2016-07-19 15:04:05 -0400

    more cleanup

--- db/migrate/20160709023243_devise_create_users.rb
+++ db/migrate/20160709023243_devise_create_users.rb
@@ -38,7 +38,6 @@ mclass DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    remove_index :users, :username
     #add_index :users, :username, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
