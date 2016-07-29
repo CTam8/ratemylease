1699c57d8951d10eef96634007aca2fb731eae42
TheMikeL <mike.l_42@hotmail.com>
2016-07-29 15:35:35 -0400

    Set up show, index and edit on controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-  def show
-  end
-
   def index
+      @users = User.paginate(page: params[:page])
+    end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
