89138b40161856f3f8a522c279157a3de212a6fe
TheMikeL <mike.l_42@hotmail.com>
2016-07-30 20:44:27 -0400

    Full name now being displayed, set up with sign up and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,14 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  #before_action :add_parameters_to_user, if: :devise_controller?
+  before_action :add_parameters_to_user, if: :devise_controller?
 
-  #def add_parameters_to_user
-  #  devise_parameter_sanitizer.for(:signup)<< :first_name
-  #  devise_parameter_sanitizer.for(:signup)<< :last_name
-  #  devise_parameter_sanitizer.for(:account_update)<< :first_name
-  #  devise_parameter_sanitizer.for(:account_update)<< :last_name
-  #end
+  def add_parameters_to_user
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name])
+    devise_parameter_sanitizer.permit(:account_update,keys: [:first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
+  end
 
   def ensure_signup_complete
       # Ensure we don't go into an infinite loop
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   #validates :username, presence: true, uniqueness: { case_sensitive: false}
-  devise :database_authenticatable, :omniauthable, :confirmable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+  devise :database_authenticatable, :omniauthable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   devise :omniauth_providers => [:facebook, :google_oauth2]
   #Authenticate using email or username
   # :confirmable,
-  def initial
-    self[0,1]
-  end
 
   has_many :reviews
 
@@ -25,7 +22,7 @@ class User < ActiveRecord::Base
       else
         user = User.new(provider: auth.provider,
           :first_name => auth.extra.raw_info.first_name,
-          :last_name => auth.extra.raw_info. last_name,
+          :last_name => auth.extra.raw_info.last_name,
           #:email => email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           :email => auth.info.email,
           #password: Devise.friendly_token[0,20]
@@ -43,6 +40,8 @@ class User < ActiveRecord::Base
 def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.provider = auth.provider
+    user.first_name= auth.first_name
+    user.last_name= auth.last_name
     user.uid = auth.uid
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
