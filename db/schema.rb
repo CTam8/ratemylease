3fc7fbdaadd88faf1f3c91b53f1525d3566678f0
TheMikeL <mike.l_42@hotmail.com>
2016-08-03 18:12:53 -0400

    Revert "General set up for avatar image"
    This reverts commit dbf2037db02c539a350f32e116fffbed05a0ea7c.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731181932) do
+ActiveRecord::Schema.define(version: 20160730231305) do
 
   create_table "fbusers", force: :cascade do |t|
     t.string   "provider"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 20160731181932) do
     t.string   "uid"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
