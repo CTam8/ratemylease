9d37a5391b24c00dacd83586c1cdc52f96121ca8
TheMikeL <mike.l_42@hotmail.com>
2016-08-03 18:12:14 -0400

    Revert " field is still nil"
    This reverts commit 94361996d0bc294e10a966aa5dd6eb78217a79e0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessor :email, :password, :avatar, :remote_avatar_url
   mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -16,7 +15,7 @@ class User < ActiveRecord::Base
   end
   has_many :reviews
 
-  def login= (login)
+  def login=(login)
     @login = login
   end
 
