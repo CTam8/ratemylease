727720998f54afe9ee81cc82c684f3c013be6343
TheMikeL <mike.l_42@hotmail.com>
2016-08-03 18:12:35 -0400

    Revert " adjustments for omniauth"
    This reverts commit ab4c1593681b19b0f49e3c9a4207fa20b4d29cf8.

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,6 @@ class User < ActiveRecord::Base
         user = User.create(provider: auth.provider,
           :first_name => auth.info.first_name,
           :last_name => auth.info.last_name,
-          :avatar => auth.info.image,
           :email => auth.info.email,
           :password => Devise.friendly_token[0,20]
         )
@@ -44,7 +43,7 @@ class User < ActiveRecord::Base
           user.first_name = auth.info.first_name
           user.last_name = auth.info.last_name
           user.email = auth.info.email
-          user.avatar = auth.info.image
+          #remote_avatar_url: auth.info.image
           user.password = Devise.friendly_token[0,20]
       end
   end
